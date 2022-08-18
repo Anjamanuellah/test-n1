38f5c41c3fef6ae3bb73d5d716b9bb1862b27a82
Anjamanuellah <manuellahanja@gmail.com>
2022-08-18 04:33:11 +0300

    test1

---
+++ test.js
@@ -0,0 +1,19 @@
+var tab1 = [2,6,3,4,9,10]
+//console.log(tab1);
+
+return resultat = tab1 * 2;
+
+function showlast(tab1){
+    return tab1(tab1(length-1));
+}
+
+var tab1 = [2,6,3,4,9,10];
+for (var k = 0 ; k <= tab1.length;k++);
+var resultat = tab1[k*2];
+
+ var tab1 = [2,6,3,4,9,10];
+ for (let i = 0;i <= tab1.length ;i--){
+    
+    console.log(tab1(length-1))
+ 
+ }
